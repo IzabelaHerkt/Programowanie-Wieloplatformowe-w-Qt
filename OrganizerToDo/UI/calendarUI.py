7e4976ea85c8e007603a612a27660d9e12f062b2
matwypych <62069546+matwypych@users.noreply.github.com>
2020-12-02 19:58:14 +0100

    Add files via upload
    -Dodanie interfejsów dla zadania i kalendarza

---
+++ OrganizerToDo/UI/calendarUI.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'calendar.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Calendar")
+        Form.resize(454, 359)
+        self.calendarWidget = QtWidgets.QCalendarWidget(Form)
+        self.calendarWidget.setGeometry(QtCore.QRect(80, 60, 312, 183))
+        self.calendarWidget.setObjectName("calendarWidget")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Calendar", "Calendar"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    widget = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(widget)
+    widget.show()
+    sys.exit(app.exec_())
