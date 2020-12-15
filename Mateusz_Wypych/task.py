90a1c5091540de02b2dbba74053d72a469141f09
matwypych <62069546+matwypych@users.noreply.github.com>
2020-12-15 21:41:45 +0100

    Add files via upload
    Pracę domową związaną z dodawaniem obiektów do listy zrealizowałem od razu w naszej aplikacji Organizer. Udostępniam tutaj klasę spełniającą podane funkcjonalności zadane w P.D z wyjątkiem max liczby obiektów oraz progress bara, ponieważ w naszej aplikacji zrealizowane to zostało jako numer wyświetlający się przed zadaniem, a progress bar występuje po podaniu prawidłowego hasła i loginu. W razie potrzeby mogę stworzyć oddzielne projekty z zadanym P.D natomiast chciałem po prostu wykorzystać od razu nabytą wiedzą do naszej aplikacji.

---
+++ Mateusz_Wypych/task.py
@@ -0,0 +1,80 @@
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget, QMessageBox
+from models.Task import Task
+
+from UI.taskUI import UI_Task
+from models.Task import Task
+
+
+class TaskMenu(QWidget, UI_Task):
+    def __init__(self, parent=None):
+        super(TaskMenu, self).__init__(parent)
+        self.setupUi(self)
+        self.Tasks = []
+        self.pushButton.clicked.connect(self.addBtn)
+        self.listView.clicked.connect(self.checkZad)
+        self.pushButton3.clicked.connect(self.editZad)
+        self.pushButton_2.clicked.connect(self.deleteZad)
+        self.objCounter = 0
+        self.hash = 0
+
+    def addTask(self, task):
+        self.Tasks.append(str(task.day) + str(task.month) + str(task.year))
+
+    def ShowApp(self):
+        self.show()
+
+    def addBtn(self):
+        zad = self.textEdit.toPlainText()
+        if zad == "":
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setWindowTitle('Uwaga!')
+            msg.setText('Nie wpisales zadnego zadania')
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+        else:
+            self.Tasks.append(zad)
+            self.textEdit.clear()
+            self.listView.insertItem(self.objCounter, str(self.objCounter) + " : " + zad)
+            self.objCounter = self.objCounter+1
+
+    def checkZad(self):
+        SelectedText = self.listView.currentItem().text()
+        self.textEdit.setText(SelectedText)
+
+    def addHash(self, hsh):
+        self.hash = hsh
+
+    def editZad(self):
+        zad = self.textEdit.toPlainText()
+        if zad != "" and self.listView.currentItem() is not None:
+            self.listView.currentItem().setText(zad)
+        elif self.listView.currentItem() is None:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setWindowTitle('Uwaga!')
+            msg.setText('Nie wybrałeś żadnego elementu')
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+        elif zad != "":
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setWindowTitle('Uwaga!')
+            msg.setText('Blad!')
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+
+    def deleteZad(self):
+        if self.listView.currentItem() is not None:
+            self.listView.currentItem().setHidden(True)
+            self.textEdit.clear()
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setWindowTitle('Uwaga!')
+            msg.setText('Nie wybrałeś żadnego elementu')
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
