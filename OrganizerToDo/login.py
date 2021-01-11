568dc5f09e8209ae55c039e476ad7f4189b9bf36
Izabela <73602256+IzabelaHerkt@users.noreply.github.com>
2021-01-11 21:23:57 +0100

    Add files via upload
    Dodano kodowanie hasła

--- OrganizerToDo/login.py
+++ OrganizerToDo/login.py
@@ -1,43 +1,59 @@
 import sys
 
+from PyQt5.QtCore import QSize
+from PyQt5.uic.properties import QtGui
+
 from UI.loginUI import loginUI
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox
 
 from models.User import User
 from ProgressBar import ProgressBarLogin
+from newuser import newUser
 
 
 class Login(QDialog, loginUI):
 
     def __init__(self, parent=None):
         super(Login, self).__init__(parent)
-        self.progressBar = ProgressBarLogin()
         self.setupUi(self)
+        self.progressBar = ProgressBarLogin()
+        self.NewUser = newUser()
 
         self.loginbtn.clicked.connect(self.loginF)
-        self.exitbtn.clicked.connect(self.exitF)
+        self.newbtn.clicked.connect(self.newF)
 
-        Admin = User("admin", "admin")
+        #Admin = User("admin", "admin")
+
+    def showApp(self):
+        self.show()
+
+    def newF(self):
+        self.NewUser.showApp()
+        self.hide()
 
     def loginF(self):
+
+        import hashlib
 
         notFound = True
         login = self.loginId.text()
         haslo = self.passwordId.text()
+        haslo = hashlib.md5(haslo.encode('utf-8')).hexdigest()
 
         filepath = 'data.txt'
 
         DELIMITER = " "
 
-        with open(filepath) as fr:
-            for line in fr:
+        with open(filepath, 'r') as plik:
+            for linia in plik.readlines():
+                linia = linia.strip()
 
-                loginAdmin = line.split("{}".format(DELIMITER))[0] #czytanie pierwszej kolumny
-                hasloAdmin = line.split("{}".format(DELIMITER))[1] #czytanie drugiej kolumny
+                loginAdmin = linia.split("{}".format(DELIMITER))[0] #czytanie pierwszej kolumny
+                hasloAdmin = linia.split("{}".format(DELIMITER))[1] #czytanie drugiej kolumny
 
                 if login == loginAdmin and haslo == hasloAdmin:
-                    print(f'Login : {login}\nHasło : {haslo}')
+                    #print(f'Login : {login}\nHasło : {haslo}')
                     print("Zalogowano!")
 
                     notFound = False
@@ -56,16 +72,17 @@ class Login(QDialog, loginUI):
                 msg.setStandardButtons(QMessageBox.Ok)
                 msg.exec_()
 
-    def exitF(self):
-        print("Zamknięto aplikację")
-        sys.exit(1)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainwindow = Login()
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(mainwindow)
+    widget.setFixedWidth(400)
+    widget.setFixedHeight(420)
+    widget.show()
+    sys.exit(app.exec())
 
 
-app = QApplication(sys.argv)
-mainwindow = Login()
-widget = QtWidgets.QStackedWidget()
-widget.addWidget(mainwindow)
-widget.setFixedWidth(400)
-widget.setFixedHeight(350)
-widget.show()
-app.exec_()
+
+
